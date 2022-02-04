a7f1560a106f577c937d67f2bb30631cef30cd0b
mortiz95 <53878835+mortiz95@users.noreply.github.com>
2022-02-04 22:09:59 +0000

    comit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import {Router,Request,Response} from 'express';
 const router = Router();
 
 router.get('/',(req:Request,res: Response ) => {
-res.render('index');
+res.render('main');
 })
 
 export default router;
